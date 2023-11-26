381a5884c0441a406073efcd87ee46964089716a
vincevise <vincentvuram204@gmail.com>
2023-11-27 01:15:31 +0530

    added the action

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,2 +1,1 @@
-ssh -t -i "100xdev.pem" ubuntu@ec2-13-233-39-120.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
-
+ssh -t -i "100xdev.pem" ubuntu@ec2-3-108-249-152.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
